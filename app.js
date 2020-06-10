e8682275fcc6d1e8b221676d4c5733a8838b04f0
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-06-10 11:07:36 +0700

    chore: remove unused SINGLE_PAGE_APP env and morgan

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const express = require("express");
-const logger = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const compression = require("compression");
 const path = require("path");
 
 const buildPath = process.env.BUILD_PATH;
-const spa =
-  process.env.SINGLE_PAGE_APP && JSON.parse(process.env.SINGLE_PAGE_APP);
 const corsConfigs = require("./configs/cors");
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(compression());
 app.use(cors(corsConfigs));
 app.use(helmet());
 
 app.use(express.static(path.join(__dirname, buildPath)));
 
-if (spa) {
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, buildPath, "index.html"));
-  });
-}
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, buildPath, "index.html"));
+});
 
 module.exports = app;
