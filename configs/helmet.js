04dcc9c2b8920712da5f96ff164e005309899e4d
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-06-14 10:24:08 +0700

    feat(helmet): update security headers and add csp

--- configs/helmet.js
+++ configs/helmet.js
@@ -2,7 +2,6 @@ const yaml = require("js-yaml");
 const fs = require("fs");
 
 const csp = yaml.safeLoad(fs.readFileSync("./csp.yml", "utf8"));
-csp.directives.fontSrc = process.env.CSP_FONT_SRC || "'self'";
 csp.directives.scriptSrc = process.env.CSP_SCRIPT_SRC || "'self'";
 csp.directives.connectSrc = process.env.CSP_CONNECT_SRC || "'self'";
 
